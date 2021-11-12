48bcbe878d2c4049cba11ed643659f5b7fe698df
MonkeyDlooooo <94170905+MonkeyDlooooo@users.noreply.github.com>
2021-11-12 17:54:18 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const nameGirl = 'Bé yêu anh yêu em';
-const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc mừng sinh nhật'
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc bé yêu của anh 1 ngày sinh nhât tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
-const giftImage = 'hot-girl.png';
-const base64 = '';
-const giftImageBase64 = "data:image/png;base64, " + base64;
